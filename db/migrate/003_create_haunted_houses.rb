f01329b15fb7502e0cf5c22813f49c9d81a59448
Xiaochen Xu <xiaochex@gmail.com>
2021-06-10 00:26:09 -0400

    commit tree 9bc9d95eaa92ffc6beb18d3f90f4b2cf02f26cd0

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,20 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.datetime :opening_time
+            t.datetime :closing_time
+            t.text :long_description
+
+            t.timestamps null: false
+        end
+    end
+end
+
